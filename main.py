e0ee5a3336b8623e583dcf1f256059cf47c86dbf
kana <kana-github@qnet.li>
2025-02-25 07:08:37 +0400

    trim emojis from title, add emoji to group title

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from os import getenv
 from dotenv import load_dotenv
 from pyrogram import Client, types, enums
@@ -5,6 +6,23 @@ from pyrogram import Client, types, enums
 load_dotenv()
 app = Client(name="playdate-games-report", api_id=getenv("API_ID"), api_hash=getenv("API_HASH"))
 
+emoji_pattern = re.compile("["
+    u"\U0001F600-\U0001F64F"
+    u"\U0001F300-\U0001F5FF"
+    u"\U0001F680-\U0001F6FF"
+    u"\U0001F1E0-\U0001F1FF"
+    u"\U0001F1F2-\U0001F1F4"
+    u"\U0001F1E6-\U0001F1FF"
+    u"\U0001F600-\U0001F64F"
+    u"\U00002702-\U000027B0"
+    u"\U000024C2-\U0001F251"
+    u"\U0001f926-\U0001f937"
+    u"\U0001F1F2"
+    u"\U0001F1F4"
+    u"\U0001F620"
+    u"\u200d"
+    u"\u2640-\u2642"
+"]+", flags=re.UNICODE)
 
 async def main():
     async with app:
@@ -21,10 +39,15 @@ async def main():
             if not message.caption:
                 continue
 
-            title = message.caption.split("\n")[0].strip()
+            title = message.caption.split("\n")[0]
+            title = emoji_pattern.sub(r'', title)
+            title = title.strip()
+
             link = message.link
 
-            game = (title, link)
+            loved = "#love" in message.caption
+
+            game = (title, link, loved)
 
             if "#in_progress" in message.caption:
                 in_progress.append(game)
@@ -45,19 +68,21 @@ async def main():
 
         def append_game(game):
             nonlocal text
-            text += f"`- `{game[0]} [↗]({game[1]})\n"
+            (title, link, loved) = game
+            loved = {True: "❤️ ", False: ""}[loved]
+            text += f"`- `{loved}{title} [↗]({link})\n"
 
         def append_title(title):
             nonlocal text
             text += f"\n**{title}**\n"
 
-        for (title, games) in [("In Progress", in_progress), ("Completed", completed), ("Todo", todo), ("Dropped", dropped), ("Scoring", scoring), ("Apps", apps)]:
+        for (title, games) in [("▶️ In Progress", in_progress), ("✅ Completed", completed), ("📋 Todo", todo), ("❌ Dropped", dropped), ("🏁 Scoring", scoring), ("⚙️ Apps", apps)]:
             games.reverse()
             append_title(title)
             for game in games:
                 append_game(game)
 
-        print(len(text))
+        print(str(int(len(text) / 4096 * 100)) + "%")
 
         await app.edit_message_text(
             chat_id=getenv("CHANNEL_ID"),
